df779107f3ea64766bbf6e760d2fa0c424c1c378
Marcelo Henrique <alvesmarcelo@discente.ufg.br>
2022-11-01 11:34:22 -0300

    Adicionando função de inserção na fila

--- queue.c
+++ queue.c
@@ -16,11 +16,35 @@ Queue *create_queue(){
     Queue *q = (Queue*)malloc(sizeof(Queue));
     if (q != NULL){
         q->head = NULL;
-        q->tam = 0;
+        q->size = 0;
     }
     else{
         printf("A memoria nao foi alocada com sucesso");
     }
+}
+void insere_final(Queue *q, int dado){
+    No *new = (No*)malloc(sizeof(No));
+    No *aux = q->head;
+    if (new == NULL){
+        printf("No nao foi alocado com sucesso");
+        return;
+    }
+    if (q->head == NULL){
+        q->head = new;
+        new->data = dado;
+        new->prox = NULL;
+        q->size++;
+    }
+    else{
+        while (aux->prox != NULL){
+            aux = aux->prox;
+        }
+        aux->prox = new;
+        new->prox = NULL;
+        new->data = dado;
+        q->size++;
+    }
+
 }
 int main(){
 
